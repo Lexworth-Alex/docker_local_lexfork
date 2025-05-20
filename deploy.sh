bcc4457ebee08ae326048a7f11af2962fd6d7b45
Lexworth-Alex <alex.okezie54@gmail.com>
2025-05-20 09:36:09 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,17 @@
 
 echo "Deploying webapp containers..."
 docker pull lexworth2539/haproxy:latest
-docker stop acada-webapp1 acada-webapp2 accada-webapp3 acada-webappp4 acada-webapp5 acada-webapp-alex || true
-docker rm acada-wepp1 acada-webapp2 acada-webapp3 acada-webapp4 acada-webapp5 acada-webapp-alex -f
+
+echo "Creating network"
 docker network create acada-app
-docker run -d --name acada-webapp1 --hostname acada-webapp1 --network acada-app lexworth2539/haproxy:latest
-docker run -d --name acada-webapp2 --hostname acada-webapp2 --network acada-app lexworth2539/haproxy:latest
-docker run -d --name acada-webapp3 --hostname acada-webapp3 --network acada-app lexworth2539/haproxy:latest
-docker run -d --name acada-webapp4 --hostname acada-webapp4 --network acada-app lexworth2539/haproxy:latest
-docker run -d --name acada-webapp5 --hostname acada-webapp5 --network acada-app lexworth2539/haproxy:latest
-docker run -d --name acada-webapp-alex --hostname acada-webapp-alex --network acada-app lexworth2539/haproxy:latest
-docker ps | grep -i acada-webapp*
-echo "Deploying HAproxy containers done and successful"
+
+for i in {1..6};
+do
+docker stop acada-webapp$i ; docker rm -f acada-webapp$i || true
+docker run -d --name acada-webapp$i --hostname acada-webapp$i --network acada-app lexworth2539/haproxy:latest;
+echo "Deploying webapp$i container done"
+done
+
 
 slep 10
 echo "Deploying HAproxy containers..."
