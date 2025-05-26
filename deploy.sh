be2bdafc979aa4ce856d0a0c6b7ba1cec4ed25cf
Lexworth-Alex <alex.okezie54@gmail.com>
2025-05-26 12:31:10 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,25 @@
 sudo mkdir -p /opt/docker_config_files/
 
 echo "Deploying webapp containers..."
-docker pull lexworth2539/haproxy:latest
+docker pull lexworth2539/acada-web:latest
 
-echo "Creating network"
-docker network create acada-app
+echo "Creating network (ignore error if exists)"
+docker network create acada-app || true
 
-for i in {1..6};
-do
-docker stop acada-webapp$i ; docker rm -f acada-webapp$i || true
-docker run -d --name acada-webapp$i --hostname acada-webapp$i --network acada-app lexworth2539/haproxy:latest;
-echo "Deploying webapp$i container done"
+# Deploy app containers
+for i in {1..6}; do
+  docker stop acada-webapp$i && docker rm -f acada-webapp$i || true
+  docker run -d --name acada-webapp$i --hostname acada-webapp$i --network acada-app lexworth2539/acada-web:latest
+  echo "Deployed acada-webapp$i"
 done
 
+sleep 10
 
-slep 10
-echo "Deploying HAproxy containers..."
-docker rm haproxy -f >/dev/null 2>&1 || true
-docker run -d --name haproxy --network acada-app -v /opt/docker_config_files/haproxy.config:/usr/local/etc/haproxy/haproxy.cfg:ro -p 9090:80 haproxy:latest
-docker ps | grep -i haproxy*
-echo "Deploying HAproxy containers done and succefull"
+echo "Deploying HAProxy container..."
+docker rm -f haproxy || true
+docker run -d --name haproxy --network acada-app \
+  -v /opt/docker_config_files/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
+  -p 8080:80 haproxy:latest
+
+docker ps | grep haproxy
+echo "HAProxy deployment done successfully"
